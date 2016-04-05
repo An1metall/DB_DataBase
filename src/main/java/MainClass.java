81cf3a6d7279b00ad01b8fcaee5ba8b46eaded06
Roman Bardashov <bardashovrv@gmail.com>
2016-04-06 00:42:52 +0300

    Using MainAppWindow as parant

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
     private static final String dataBase = "db\\data.db";
     private static final String JDBC = "jdbc:sqlite";
     private static final String dataBaseUrl = JDBC + ":" + dataBase;
+    public static MainAppWindow mainAppWindow;
 
     public static void main(String[] args) throws IOException {
         if (DataBase.connect(dataBaseUrl)) {
